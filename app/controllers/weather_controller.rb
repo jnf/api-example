030dc93b50a10142d21a44296cdff01894a9689d
jnf <jeremy.flores@gmail.com>
2015-08-03 11:29:13 -0700

    Get the weather from the weather api.

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,8 @@
+require 'httparty'
+
 class WeatherController < ApplicationController
+  WEATHER_URI = "http://api.openweathermap.org/data/2.5/weather?mode=json&units=imperial&"
+
   def index; end
 
   def city_redirect
@@ -10,8 +14,11 @@ class WeatherController < ApplicationController
   end
 
   def city
+    @city = params[:city]
+    
     # let's get some weather data from the api
-
+    response = HTTParty.get(WEATHER_URI + "q=#{@city}")
+    @current_temperature = response["main"]["temp"]
+    @number_of_clouds    = response["clouds"]["all"]
   end
 end
-
